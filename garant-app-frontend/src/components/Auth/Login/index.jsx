2edc1da9c621902f7bbf2bd0bd69553d043b9217
edis <edis.sinanovi5@gmail.com>
2024-05-17 13:11:59 +0200

    feat: create login screen

---
+++ garant-app-frontend/src/components/Auth/Login/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Logika za prijavo
+    console.log('Login with', email, password);
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h2 className={styles.title}>Login</h2>
+        <div className={styles.inputGroup}>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <br/>
+        <button type="submit" className={styles.button}>Log In</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
--- garant-app-frontend/src/index.js
+++ garant-app-frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginForm from './components/Auth/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <LoginForm/>
   </React.StrictMode>
 );
 
