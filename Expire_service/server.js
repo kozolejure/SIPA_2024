1cf139e980c4a0a80dd1eb74f8340317307eb2d1
Jure Kozole <Kozole.jure@gmail.com>
2024-05-17 13:26:01 +0200

    Fix for expire_service

--- Expire_service/server.js
+++ Expire_service/server.js
@@ -5,11 +5,11 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 
 const app = express();
-const port = 3000;
+const port = 3003;
 
 app.use(bodyParser.json());
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://admin:admin@localhost:27018/userservice?authSource=admin';
-mongoose.connect('mongodb://admin:admin@data:27017/userservice?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
