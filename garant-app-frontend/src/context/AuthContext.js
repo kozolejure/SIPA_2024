709a8affe9f45940367f6aa2211cfcd5e614d000
edis <edis.sinanovi5@gmail.com>
2024-05-23 15:42:41 +0200

    fix: add token checking - no logout after refresh

--- garant-app-frontend/src/context/AuthContext.js
+++ garant-app-frontend/src/context/AuthContext.js
@@ -1,8 +1,8 @@
 // src/context/AuthContext.js
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { saveTokens, deleteTokens } from '../utils/tokensIndexedDB';
+import { saveTokens, deleteTokens, getTokens } from '../utils/tokensIndexedDB';
 import { jwtDecode } from 'jwt-decode';
 
 const AuthContext = createContext();
@@ -10,6 +10,29 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkUser = async () => {
+            const tokens = await getTokens();
+
+            if (tokens && tokens.jwtToken) {
+                try {
+                    const decodedToken = jwtDecode(tokens.jwtToken);
+                    setUser(decodedToken);
+                    navigate('/');
+
+                    if (!decodedToken) {
+                        logout();
+                    }
+                } catch (error) {
+                    console.error('Error decoding token:', error);
+                    logout();
+                }
+            }
+        };
+
+        checkUser();
+    }, []);
 
     const login = async (username, password) => {
         try {
