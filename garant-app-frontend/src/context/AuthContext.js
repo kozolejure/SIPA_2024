fa181b17e4cef57dc403ce0a747d3f3877aa9d86
edis <edis.sinanovi5@gmail.com>
2024-06-10 12:42:52 +0200

    fix: registration witouth tokens

--- garant-app-frontend/src/context/AuthContext.js
+++ garant-app-frontend/src/context/AuthContext.js
@@ -8,99 +8,100 @@ import { jwtDecode } from "jwt-decode";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const checkUser = async () => {
-      const tokens = await getTokens();
+    useEffect(() => {
+        const checkUser = async () => {
+            const tokens = await getTokens();
 
-      if (tokens && tokens.jwtToken) {
+            if (tokens && tokens.jwtToken) {
+                try {
+                    const decodedToken = jwtDecode(tokens.jwtToken);
+                    setUser(decodedToken);
+                    navigate("/");
+
+                    if (!decodedToken) {
+                        logout();
+                    }
+                } catch (error) {
+                    console.error("Error decoding token:", error);
+                    logout();
+                }
+            } else if (window.location.pathname !== '/registration') {
+                logout();
+                navigate('/login');
+            }
+        };
+
+        checkUser();
+    }, []);
+
+    const login = async (username, password) => {
         try {
-          const decodedToken = jwtDecode(tokens.jwtToken);
-          setUser(decodedToken);
-          navigate("/");
+            const response = await axios.post("http://localhost:3001/login", {
+                username,
+                password,
+            });
+            const decodedToken = jwtDecode(response.data.token);
+            saveTokens(response.data.token, response.data.refreshToken);
+            setUser(decodedToken);
 
-          if (!decodedToken) {
-            logout();
-          }
+            const isFirstTimeLogin = await checkIfIsFirstTimeLogin(
+                response.data.user.id
+            );
+            console.log("isFirstTimeLogin: " + isFirstTimeLogin);
+
+            if (isFirstTimeLogin) {
+                navigate("/first-login");
+                return;
+            } else {
+                console.log(response.data);
+                navigate("/");
+            }
+
+            navigate("/");
         } catch (error) {
-          console.error("Error decoding token:", error);
-          logout();
+            console.error("Login failed:", error);
+            throw error;
         }
-      } else {
-        logout();
-        navigate("/login");
-      }
     };
 
-    checkUser();
-  }, []);
-
-  const login = async (username, password) => {
-    try {
-      const response = await axios.post("http://localhost:3001/login", {
-        username,
-        password,
-      });
-      const decodedToken = jwtDecode(response.data.token);
-      saveTokens(response.data.token, response.data.refreshToken);
-      setUser(decodedToken);
-
-      const isFirstTimeLogin = await checkIfIsFirstTimeLogin(
-        response.data.user.id
-      );
-      console.log("isFirstTimeLogin: " + isFirstTimeLogin);
-
-      if (isFirstTimeLogin) {
-        navigate("/first-login");
-        return;
-      } else {
-        console.log(response.data);
-        navigate("/");
-      }
-
-      navigate("/");
-    } catch (error) {
-      console.error("Login failed:", error);
-      throw error;
-    }
-  };
-
-  const checkIfIsFirstTimeLogin = async (userId) => {
-    try {
-      console.log("Checking if is first time login for user:", userId);
-      const response = await axios.get(
-        `http://localhost:3002/users/${userId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${tokens.jwtToken}`,
-          },
+    const checkIfIsFirstTimeLogin = async (userId) => {
+        try {
+            console.log("Checking if is first time login for user:", userId);
+            const response = await axios.get(
+                `http://localhost:3002/users/${userId}`,
+                {
+                    headers: {
+                        // eslint-disable-next-line no-undef
+                        Authorization: `Bearer ${tokens.jwtToken}`,
+                    },
+                }
+            );
+            console.log("Response status:", response.status); // This will show the actual response status code
+            return response.status === 404;
+        } catch (error) {
+            console.error("Error checking user's first-time login status:", error);
+            if (error.response) {
+                console.log("Error response status:", error.response.status); // This will show the error status code if there is an error response
+                return error.response.status === 404; // Handle specific status code on error
+            }
+            return false;
         }
-      );
-      console.log("Response status:", response.status); // This will show the actual response status code
-      return response.status === 404;
-    } catch (error) {
-      console.error("Error checking user's first-time login status:", error);
-      if (error.response) {
-        console.log("Error response status:", error.response.status); // This will show the error status code if there is an error response
-        return error.response.status === 404; // Handle specific status code on error
-      }
-      return false;
-    }
-  };
+    };
 
-  const logout = () => {
-    deleteTokens();
-    setUser(null);
-    navigate("/login");
-  };
+    const logout = () => {
+        deleteTokens();
+        setUser(null);
+        navigate("/login");
+    };
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export const useAuth = () => useContext(AuthContext);
